d090728f90891dbc355ce387804d0913594a23e0
gaerig <mathilde.t.t2903@gmail.com>
2022-10-17 19:07:18 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
-function App() {
+export default function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} />
+  ));
+
+  console.log(allNewDice());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="titles">
+          <h1 className="app-title">Dice Game</h1>
+          <h2>
+            Choose a set of dice that have the same value and roll until every
+            dice have the same value.{" "}
+          </h2>
+        </div>
+        <div className="dice-container">{diceElements}</div>
+        <button onClick={rollDice}>Roll</button>
+      </main>
     </div>
   );
 }
-
-export default App;
